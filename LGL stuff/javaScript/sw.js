6c4aa205ba58f430a2111745f813bd4e07845ef2
Stuflo19 <55452060+Stuflo19@users.noreply.github.com>
2019-11-24 13:54:51 +0000

    PWA

---
+++ LGL stuff/javaScript/sw.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'lgl-pwa';
+var urlsToCache = [
+  '/',
+  '/map.html',
+  '/json/manifest.json'
+];
+
+self.addEventListener('install', function(event){
+
+  //perform install
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      //cache hit - return response
+      if(response) {
+        return response;
+      }
+
+      return fetch(event.request).then(
+        function(response) {
+          //check if valid response
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+          .then(function(cache){
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        }
+      );
+
+    })
+  );
+});
